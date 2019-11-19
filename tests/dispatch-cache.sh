70dd229a74568cd562e7be5937ff9d918c6d3c60
Keith Turner <kturner@apache.org>
2019-11-19 13:16:19 -0500

    script updates

--- tests/dispatch-cache.sh
+++ tests/dispatch-cache.sh
@@ -9,11 +9,16 @@ cd $TESTS_DIR/..
   echo "createtable dc"
   echo "config -s tserver.scan.executors.bge.threads=1"
   echo "config -t dc -s table.scan.dispatcher.opts.executor.background=bge"
-  echo "config -t dc -s table.scan.dispatcher.opts.cache.background=opportunistic"
+  echo "config -t dc -s table.scan.dispatcher.opts.cache.background.usage=opportunistic"
   echo "config -t dc -s table.cache.index.enable=true"
   echo "config -t dc -s table.cache.block.enable=true"
   echo "config -t dc -s table.file.compress.type=snappy"
 ) | accumulo shell -u root -p secret
+
+accumulo-cluster stop
+accumulo-cluster start
+
+mkdir -p results
 
 ./bin/run.sh cmd.Ingest dc 1000000 20 10 > results/ingest.out 2>results/ingest.err
 
@@ -22,7 +27,6 @@ accumulo shell -u root -p secret -e 'flush -t dc -w'
 # give time for any splits
 sleep 3
 
-mkdir -p results
 
 ./bin/run.sh cmd.FullScan dc 3 > results/full-scan.out 2>results/full-scan.err &
 
